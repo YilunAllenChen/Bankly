4515c2bd4755f5bb901ae315ed7198cb7391d350
YilunAllenChen <allenchen@gatech.edu>
2021-05-27 16:55:15 -0400

    better env.sh

--- k8s/env.sh
+++ k8s/env.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# usage: source env.sh
+
 export KUBECONFIG=bankly-kubeconfig.yaml
+echo $KUBECONFIG
+
 
 
 # example usages
-
-
 # kubectl get pods
 # kubectl edit deployment bankly-deployment
 # kubectl rollout restart deployment bankly-deployment
